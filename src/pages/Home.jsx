318b13b292b4b852bbf217824bba79900710b6af
meganirenegott <meganirenegott@gmail.com>
2026-01-06 23:38:31 -0800

    sorted the country cards into alphabetical order

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,21 @@
 
 
 import CountryCard from "../components/CountryCard";
+// function to make a copy of the countries array, then reorder it alphabetically based on the countries common name, without mutating the original data
+function Home({ countriesData = [] }) {
+  // sortedCountries is a new variable to hold the sorted version of the country data
+  // spread operator takes every element inside the countriesData and creates a new arrray in memory, chose to do this instead of .sort() which mutates the original array
+  // ((a,b) => is the comparison function, with a and b being the common name of two contries, this function is called many times
+  // localeCompare is the actual alphabetical comparison, a string method that compares two strings and also preserves the language rules and accents
+  const sortedCountries = [...countriesData].sort((a, b) =>
+    a.name.common.localeCompare(b.name.common)
+  );
 
-function Home({ countriesData }) {
   return (
     <div className="countries-grid">
-      {countriesData.map((country) => (
+      {sortedCountries.map((country) => (
         <CountryCard
           key={country.cca3}
-          name={country.name.common}
           country={country}
         />
       ))}
@@ -18,5 +25,4 @@ function Home({ countriesData }) {
 }
 
 export default Home;
-
 
