46af3656049ac0fe262f592888526fc45c73bb98
meganirenegott <meganirenegott@gmail.com>
2026-01-14 22:53:58 -0800

    country details page renders, css needs fixing still

--- version-1/src/pages/CountryDetails.jsx
+++ version-1/src/pages/CountryDetails.jsx
@@ -1,13 +1,17 @@
 // pages/CountryDetails.jsx
 import { useParams } from "react-router";
 import CountryCard from "../components/CountryCard";
+import "../App.css";
+import "../SavedCountries.css";
 
 function CountryDetails({ countriesData }) {
   // get this country's name from the URL parameter
   const countryName = useParams().countryName;
-  console.log(countryName);
+  // search for a matching country in countriesData that has the same name as the param being passed in
   const userSelectedCountry = countriesData.find((countryInfo) => (countryInfo.name.common === countryName))
-  console.log(userSelectedCountry.name.common);
+  // we assume a match will always be found (no try catch)
+  /* place our countryCard componenet passing in the user selected countries information,
+       parameters key is userSelectedCountry.cca3 because this is unique to each country, country is userSelectedCountry   */
   return (
     <>
       <h1>Country Details Page</h1>;
