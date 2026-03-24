29e21d746503019dde4e85f9bf50b5dcc4eb7a02
meganirenegott <meganirenegott@gmail.com>
2026-03-23 23:07:12 -0700

    testing API endoints

--- version-3/server/src/index.js
+++ version-3/server/src/index.js
@@ -25,14 +25,61 @@ app.listen(port, () => {
 
 // users
 // 1.🔹 GET /get-newest-user 
-// 2.🔹 GET /get-all-users  
 
+async function getNewestUser() {
+    const result = await db.query(
+    "SELECT name FROM users ORDER BY user_id DESC LIMIT 1");
+    return result.rows;
+}
+// 2.🔹 GET /get-all-users
+
+async function getAllUsers() {
+  const result = await db.query("SELECT * FROM users;");
+  return result.rows;
+}
 
 // 3.🔹 POST /add-one-user  
 
+async function addOneUser(
+    name,
+    country_name,
+    email,
+    bio,
+) {
+    const result = await db.query(
+     `INSERT INTO users (name, country_name, email, bio)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *`,
+        [
+            name,
+            country_name,
+            email,
+            bio,
+        ],
+    );
+
+    return result.rows[0];
+}
+
 //  SAVED COUNTRIES
 // 4.🔹 GET /get-all-saved-countries  
+
+async function getAllSavedCountries() {
+    const result = await db.query("SELECT country_name FROM saved_countries;"
+    [country_name],
+);
+  return result.rows;
+}
+
 // 5.🔹 POST /save-one-country  
+
+async function saveOneCountry(
+    country_name) {
+    const result = await db.query(`SELECT saved_country_id FROM saved_countries WHERE country_name = $1`);
+    return result.rows;
+};
+
+
 // 6.🔹 POST /unsave-one-country  
 
 //  COUNTRY COUNTS
@@ -44,12 +91,64 @@ app.listen(port, () => {
 
 // users
 // 1.🔹 GET /get-newest-user 
+
+app.get("/get-newest-user", async (req, res) => {
+  // calls the helper function
+  const user = await getNewestUser();
+  res.json(user);
+});
+
+
 // 2.🔹 GET /get-all-users  
+
+app.get("/get-all-users", async (req, res) => {
+  const allUsers = await getAllUsers();
+  res.json(allUsers);
+});
+
 // 3.🔹 POST /add-one-user  
+
+
+app.post("/add-one-user", async (req, res) => {
+  const {
+    name,
+    country_name,
+    email,
+    bio,
+  } = req.body;
+
+  const user = await addOneUser(
+    name,
+    country_name,
+    email,
+    bio,
+  );
+
+  res.send(`Success! ${user.name} was added!`);
+});
+
 
 // SAVED COUNTRIES
 // 4.🔹 GET /get-all-saved-countries  
+
+app.get("/get-all-saved-countries", async (req, res) => {
+  const allCountries = await getAllSavedCountries();
+  res.json(allCountries);
+});
+
+
 // 5.🔹 POST /save-one-country  
+
+app.post("/save-one-country/:country", async (req, res) => {
+    const {
+    country_name,
+    } = req.body;
+
+    const savedCountry = await saveOneCountry(country_name);
+
+  res.send(`Success! ${savedCountry.country_name} was saved!`);
+});
+
 // 6.🔹 POST /unsave-one-country  
 
 // COUNTRY COUNTS
